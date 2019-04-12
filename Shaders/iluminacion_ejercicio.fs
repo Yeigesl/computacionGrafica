5fa16b396f5077ca00d513245be1206bbeea433b
Yareli <elizabeth.yare2704@gmail.com>
2019-04-12 08:51:08 +0200

    Se aañadio el material de rubí, se modificron los valores de las componenentes ambientales

--- Shaders/iluminacion_ejercicio.fs
+++ Shaders/iluminacion_ejercicio.fs
@@ -16,13 +16,14 @@ uniform Positionlight light;
 uniform vec3 viewPos;
 void main(){
 	vec3 ambient= light.ambient*our_color;
+	
 	vec3 normal =normalize(our_normal);
 	vec3 lightDir=normalize(light.position-fragPos);
 	float diff = max(dot(lightDir,normal),0.0);
     vec3 diffuse = diff*light.diffuse*our_color;
-	// negativo se refleja desde arriba hacia la normal
+	// negativo se refleja desde arriba hacia la normal-- comentario
 	vec3 r= reflect(-lightDir,normal);
-	//calucalo de la posicion del observador al fragemento 
+	//calucalo de la posicion del observador al fragemento  --comentario
 	vec3 viewDir = normalize(viewPos-fragPos);
 	float spec =pow(max(dot(r,viewDir),0.0),128.0); // 32 COMONENTE DE BRILLO mas grande la potencia más fina es la cirnfencia 
 	vec3 specular =spec*light.specular*our_color;
